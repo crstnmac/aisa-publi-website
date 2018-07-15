9efd22fead708f313b80b6ffc7f453e2cf6ebd97
marinaaisa <marina@marinaaisa.com>
2018-07-15 22:42:51 +0200

    Fix style in work component

--- components/WorkType.vue
+++
@@ -1,8 +0,0 @@
-.work-type {
-  &__description {
-    @include make-sm-column(4);
-  }
-  &__content {
-    @include make-sm-column(8);
-  }
-}
--- components/sections/Work.vue
+++ components/sections/Work.vue
@@ -97,4 +97,12 @@ export default {
     box-shadow: none;
     z-index: 2;
 }
+.work-type {
+  &__description {
+    @include make-sm-column(4);
+  }
+  &__content {
+    @include make-sm-column(8);
+  }
+}
 </style>
