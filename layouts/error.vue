e79c397003916aff7834021d2be074c62ab01106
marinaaisa <marina@marinaaisa.com>
2018-09-06 22:48:53 +0200

    Add error page

---
+++ layouts/error.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+  <h1>ERROR 404</h1>
+  <p>Esta URL no existe, será mejor que vuelvas al <a href="/">inicio</a> para no perderte ;)</p>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
